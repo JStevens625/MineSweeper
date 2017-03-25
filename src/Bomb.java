0b60f48ec6e322ba6e76f52715581f098b34d62a
Scott Shriver <sshriver2380@flpoly.org>
2017-03-25 16:50:37 -0400

    Began implementing abstract Tile class and its children Bomb and Empty

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,7 @@
  */
 public class Bomb extends Tile
 {
-    
+    public void selected() {
+        JOptionPane.showMessageDialog(null, "This tile was a bomb.");
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -10,5 +10,5 @@
  */
 public abstract class Tile
 {
-    
+    public abstract void selected();
 }
