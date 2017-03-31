f025e56e9b95899290b9594f31cbeeeb5abc53a9
Jacob Stevens <cobymstevens44@gmail.com>
2017-03-30 20:46:57 -0400

    Added Jbutton ActionListeners and moved the instructions to Minesweeper file

--- src/MineSweeperGame.java
+++ src/MineSweeperGame.java
@@ -1,13 +1,23 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,24 +28,51 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  *
  * @author Jacob Stevens
  */
-public class MineSweeperGame extends JFrame
+public class MineSweeperGame
 {
     Clip music;
     AudioInputStream ais;
+    private static int Boardpick = 0;
+
+    public static void setBoardpick(int Boardpick)
+    {
+        MineSweeperGame.Boardpick = Boardpick;
+    }
+
+    public int getBoardpick()
+    {
+        return Boardpick;
+    }
+    
+    private ActionListener easygame = new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            MineSweeperGame.setBoardpick(1);
+            System.out.println(Boardpick);
+        }
+    };
+    
+    private ActionListener mediumgame = new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            MineSweeperGame.setBoardpick(2);
+            System.out.println(Boardpick);
+        }
+    };
+    
+    private ActionListener hardgame = new ActionListener()
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            MineSweeperGame.setBoardpick(3);
+            System.out.println(Boardpick);
+        }
+    };
+    
     public MineSweeperGame()
     {
-        //Full MainFrame
-        setTitle("SpaceBalls MineSweeper");
-        setSize(700, 600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        //Aligns window to center of screen. Found it here: http://stackoverflow.com/questions/2442599/how-to-set-jframe-to-appear-centered-regardless-of-monitor-resolution
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
-        
-        //Adding all panels to frame
-        
-
         //Music
         try {
             music = AudioSystem.getClip();
@@ -43,12 +80,56 @@ public class MineSweeperGame extends JFrame
             music.open(ais);
             music.loop(Clip.LOOP_CONTINUOUSLY);
         } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {}
+        
+        //Instructions Screen and mode chooser
+        JFrame mode = new JFrame();
+        mode.setSize(600, 600);
+        Font myfont = new Font("Serif", 0, 20);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        mode.setLocation(dim.width / 2 - mode.getSize().width / 2, dim.height / 2 - mode.getSize().height / 2);
+        mode.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mode.setLayout(new GridLayout(3,1,0,0));
+        JTextArea jt = new JTextArea("Minesweeper is a single-player game where you try to make it through a mine field with out hitting any bombs. If you hit a bomb you lose. If you hit a space without a bomb it will give you a number, the number symbolizes how many bombs are directly around it. To begin click on one of the difficulties below to start. Beginner will start with a 9X9 Board and have 10 bombs. Intermediate will have a 16X16 Board and have 40 bombs. And Advanced will have a 16X30 Board with 99 bombs.",20,20);
+        JPanel jp = new JPanel(new GridLayout(1, 3));
+        jt.setLineWrap(true);
+        jt.setWrapStyleWord(true);
+        jt.setFont(myfont);
+        jt.setEditable(false);
+        JButton easy = new JButton();
+        JButton medium = new JButton();
+        JButton hard = new JButton();
+        easy.setIcon(new ImageIcon("src/Difficulties/EasyModedone.jpg"));
+        medium.setIcon(new ImageIcon("src/Difficulties/MediumModedone.jpg"));
+        hard.setIcon(new ImageIcon("src/Difficulties/HardModefixdone.jpg"));
+        easy.addActionListener(easygame);
+        medium.addActionListener(mediumgame);
+        hard.addActionListener(hardgame);
+        jp.add(easy);
+        jp.add(medium);
+        jp.add(hard);
+        mode.add(new JLabel(new ImageIcon("src/Pictures/Intro.jpg")), BorderLayout.NORTH);
+        mode.add(jt, BorderLayout.CENTER);
+        mode.add(jp, BorderLayout.SOUTH);
+        mode.setVisible(true); 
+        //end of instructions screen and mode chooser
+        
+        
+        
+        //Game MainFrame
+        JFrame GameFrame = new JFrame();
+        GameFrame.setTitle("SpaceBalls MineSweeper");
+        GameFrame.setSize(700, 600);
+        GameFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Aligns window to center of screen. Found it here: http://stackoverflow.com/questions/2442599/how-to-set-jframe-to-appear-centered-regardless-of-monitor-resolution
+        GameFrame.setLocation(dim.width / 2 - GameFrame.getSize().width / 2, dim.height / 2 - GameFrame.getSize().height / 2);
+
 
         // Beginning of Colors
         
         //End of Colors
-
-        setVisible(false);
-        setResizable(false);
+        
+        GameFrame.setVisible(false);
+        GameFrame.setResizable(false);
     }
 }
