70b651b4ad1a598e34d37ab3f543563adb4a135a
Jacob Stevens <cobymstevens44@gmail.com>
2017-04-21 22:05:22 -0400

    Implemented JButtons in tile class

--- src/Tile.java
+++ src/Tile.java
@@ -2,53 +2,82 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import sun.nio.cs.ext.MS874;
 
 /**
  *
  * @author Jacob Stevens and Scott Shriver
  */
-public abstract class Tile extends JButton implements ActionListener
+public abstract class Tile implements ActionListener
 {
 
     MineSweeperGame ms = new MineSweeperGame();
     Tile easytile[][];
     Tile mediumtile[][];
     Tile hardtile[][];
+    JButton easybuttons[][] = new JButton[9][9];
+    JButton mediumbuttons[][] = new JButton[16][16];
+    JButton hardbuttons[][] = new JButton[30][16];
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
         if (ms.getDifficultsetter() == 1) {
-            for (int i = 0; i < easytile.length; i++) {
-                for (int j = 0; j < easytile.length; j++) {
-                    if (e.getSource() == easytile[i][j]) {
-                        easytile[i][j].setEnabled(false);
+            for (int i = 0; i < easybuttons.length; i++) {
+                for (int j = 0; j < easybuttons.length; j++) {
+                    if (e.getSource() == easybuttons[i][j]) {
+                        easybuttons[i][j].setEnabled(false);
                     }
                 }
             };
         }
         if (ms.getDifficultsetter() == 2) {
-            for (int i = 0; i < mediumtile.length; i++) {
-                for (int j = 0; j < mediumtile.length; j++) {
-                    if (e.getSource() == mediumtile[i][j]) {
-                        mediumtile[i][j].setEnabled(false);
+            for (int i = 0; i < mediumbuttons.length; i++) {
+                for (int j = 0; j < mediumbuttons.length; j++) {
+                    if (e.getSource() == mediumbuttons[i][j]) {
+                        mediumbuttons[i][j].setEnabled(false);
                     }
                 }
             };
         }
         if (ms.getDifficultsetter() == 3) {
-            for (int i = 0; i < hardtile.length; i++) {
-                for (int j = 0; j < hardtile.length; j++) {
-                    if (e.getSource() == hardtile[i][j]) {
-                        hardtile[i][j].setEnabled(false);
+            for (int i = 0; i < hardbuttons.length; i++) {
+                for (int j = 0; j < hardbuttons.length; j++) {
+                    if (e.getSource() == hardbuttons[i][j]) {
+                        hardbuttons[i][j].setEnabled(false);
                     }
                 }
             };
         }
     }
-    
-    public abstract
+    public void easygame(JPanel jpanel){
+        for (int i = 0; i < 9; i++) {
+            for (int k = 0; k < 9; k++) {
+                    
+                easybuttons[i][k].addActionListener(this);
+                jpanel.add(easybuttons[i][k]);
+            }
+        }
+    }
+    public void mediumgame(JPanel jpanel){
+        for (int i = 0; i < 16; i++) {
+            for (int k = 0; k < 16; k++) {
+                    
+                easybuttons[i][k].addActionListener(this);
+                jpanel.add(easybuttons[i][k]);
+            }
+        }
+    }
+    public void hardgame(JPanel jpanel){
+        for (int i = 0; i < 30; i++) {
+            for (int k = 0; k < 16; k++) {
+                    
+                easybuttons[i][k].addActionListener(this);
+                jpanel.add(easybuttons[i][k]);
+            }
+        }
+    }
 
     int x, y, numOfBombs = 0;
     boolean hasBeenClicked = false;
